c7710849c33525bff5c067e2f2718844155c6f6f
xushiwei <xushiweizh@gmail.com>
2020-04-13 01:50:00 +0800

    github.com/qiniu/audio/mp3: use bufiox.NewReader

--- mp3/decode.go
+++ mp3/decode.go
@@ -4,24 +4,28 @@ import (
 	"io"
 
 	"github.com/qiniu/audio"
+	"github.com/qiniu/x/bufiox"
 
 	mp3 "github.com/hajimehoshi/go-mp3"
 )
 
 // -------------------------------------------------------------------------------------
 
-func decode(r io.ReadSeeker) (audio.Decoded, error) {
-	dec, err := mp3.NewDecoder(r)
+// Decode decodes a mp3 audio.
+func Decode(r io.ReadSeeker) (audio.Decoded, error) {
+	b := bufiox.NewReader(r)
+	dec, err := mp3.NewDecoder(b)
 	return dec, err
 }
 
-func decodeConfig(r io.ReadSeeker) (cfg audio.Config, err error) {
+// DecodeConfig is not implemented.
+func DecodeConfig(r io.ReadSeeker) (cfg audio.Config, err error) {
 	err = audio.ErrFormat
 	return
 }
 
 func init() {
-	audio.RegisterFormat("mp3", "ID3", decode, decodeConfig)
+	audio.RegisterFormat("mp3", "ID3", Decode, DecodeConfig)
 }
 
 // -------------------------------------------------------------------------------------
